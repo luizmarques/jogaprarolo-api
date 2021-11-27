eeb83c9564fcae995d1030d0ead5bee057f9963f
LuizPC <luiz.marques@al.infnet.edu.br>
2021-11-27 11:44:50 -0300

    heroku deploy

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ app.use(cors());
 
 app.use(router);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
